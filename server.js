a72c9f3b1f5122e3ec1bda8de113782194a920c2
Cesur Tanik <tanikcesur@gmail.com>
2026-09-07 18:11:25 +0300

    Prepare backend for deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -168,6 +168,10 @@ app.get("/api/asteroids", async (req, res) => {
 // SERVER
 // ====================
 
-app.listen(PORT, () => {
-  console.log(`NASA backend çalışıyor: http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`NASA backend çalışıyor: ${PORT}`);
 });
+
+setInterval(() => {
+  console.log("Backend çalışıyor...");
+}, 10000);
